960d8b08e9c10116f51dbf4632c5e07ef697ee1e
Jameson Hill <stevenjamesonhill@gmail.com>
2017-04-17 13:45:22 -0400

    add recipe tests

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,83 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('recipes list', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer;
+  });
+
+  it('should list recipes on GET', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.length.should.be.at.least(1);
+
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.forEach(function(item) {
+        item.should.be.a('object');
+        item.should.include.keys(expectedKeys);
+      });
+    });
+  });
+
+  it('should add recipe on POST', function() {
+    const newItem = {name:'spaghetti', ingredients: ['sauce', 'noodles', 'olive oil']}
+    return chai.request(app)
+    .post('/recipes')
+    .send(newItem)
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('name', 'ingredients', 'id');
+      res.body.id.should.not.be.null;
+      res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+    });
+
+  });
+
+  it('should update recipe on PUT', function() {
+    const updatedItem = {
+      name: 'spaghetti with meatballs',
+      ingredients: ['sauce', 'noodles', 'olive oil', 'meatballs']
+    }
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      updatedItem.id = res.body[0].id;
+
+      return chai.request(app)
+      .put(`/recipes/${updatedItem.id}`)
+      .send(updatedItem)
+    })
+
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.deep.equal(updatedItem);
+    });
+
+  });
+
+  it('should delete on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`)
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+});
